96aa32acb4497dd79d5fe8a52dcd378496a60492
Sunil <cinemaalu@live.com>
2013-02-09 00:48:28 -0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save       # Handle a successful save.
+			sign_in @user
       flash[:success] = "Welcome to the Project Explorer!"
 			redirect_to @user
     else
       render 'new'
+      flash[:notsuccess] = "User not founf please Sugnup!"
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
@@ -112,5 +113,8 @@ describe User do
     end
   end
 
-#  pending "add some examples to (or delete) #{__FILE__}"
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
